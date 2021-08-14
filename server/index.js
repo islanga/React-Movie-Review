a29276428bf0c41df5e613a57c3393aae1c50b66
innocentl <innocent.langa@net1.com>
2021-08-14 16:25:17 +0200

    First Commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "CRUDDataBase",
+});
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res) => {
+    const sqlSelect = "SELECT movieName, movieReview FROM movie_reviews";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.post("/api/insert", (req, res) => {
+
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)"
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {    
+        console.log(result);
+    });
+});
+
+app.delete("/api/delete/:movieName", (req, res) => {
+    const name = req.params.movieName
+    const sqlDelete = "DELETE FROM movie_reviews WHERE movieName = ?";
+    db.query(sqlDelete, name, (err, result) => {
+        if (err) console.log(err);
+    });
+});
+
+app.listen(3001, () => {
+    console.log("running on port 3001");
+});
